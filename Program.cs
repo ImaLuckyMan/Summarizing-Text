8c9f92a7708b7ed2d6b09b9eb1112542629acfff
ImaLuckyMan <Ima_Lucky_Man@yahoo.com>
2020-02-24 14:30:17 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Summerizing_Text 
+{
+    class Program   /* This will be used in order to summarize long text into shortend text followed by... */
+    {
+        static void Main(string[] args)
+        {
+            var sentance = "This is goint to be a really really really really really really long text"; //input string
+            var summary = StringUtility.SummerizeText(sentance, 25); // calls method in StringUtility class with 25 character input
+            Console.WriteLine(summary); // Displays results to the console
+        }
+
+        
+    }
+}
---
+++ StringUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Summerizing_Text
+{
+    public class StringUtility
+    {
+        public static string SummerizeText(string text, int maxLength = 20) // Method with 20 charactor default if one is not set
+        {
+
+            if (text.Length < maxLength) // If the text is less than length above then go ahead and print 
+                return text;
+
+            var words = text.Split(' '); // Break up the string by spaces into an array
+            var totalCharacters = 0;
+            var summaryWords = new List<string>(); // New place holder string array
+
+            foreach (var word in words) // for each word broken up by spaces do the following
+            {
+                summaryWords.Add(word); // add the word
+
+                totalCharacters += word.Length + 1; // count the characters of each word added plus 1 for the space
+                if (totalCharacters > maxLength) // when the total reaches the maxLength set then break out of the loop
+                    break;
+            }
+
+            return String.Join(" ", summaryWords) + "..."; ; //return the words that were added
+        }
+    }
+}
+    
